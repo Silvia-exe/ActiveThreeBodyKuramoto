87d38cc5c15a6b506b3420af88cfce0224a54330
Silvia <silmor1999@gmail.com>
2025-06-03 16:55:09 +0200

    Added noisy movement to Kuramoto plus some animation tweaks for smoother gifs

--- kuramoto_2d_active/directed2d_kuramoto.py
+++ kuramoto_2d_active/directed2d_kuramoto.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 import matplotlib.animation as animation
 import plotting as kplt
 import numpy as np
 import math
 
-N= 100#number of particles
-T = 50
-Lx = 10 #Box size
-K1 = 1.0 #Pairwise interaction parameter
-K2 = 0.0 #Trio-wise interaction parameter
-dt = 0.05 #Integration step
-v = 1.0 #Internal particle velocity
-t = np.linspace(0,T,int(T/dt))
+def box_muller_noise():
+    U1 = np.random.uniform()
+    U2 = np.random.uniform()
+    R = np.sqrt(-2 * np.log(U1))
+    phi = 2 * np.pi * U2
+    X = R * np.cos(phi)
+    Y = R * np.sin(phi)
+    return (X,Y)
 
+def move_noise(r_v, th):
+    new_r_v = np.copy(r_v)
+    for i in range(len(r_v)):
+        nx, ny = box_muller_noise()
+        dx = v*dt*np.cos(th[i]) + 1/gamma * nx
+        dy = v*dt*np.sin(th[i]) + 1/gamma * ny
+        #dx = v*dt + 1/gamma * nx
+        #dy = v*dt + 1/gamma * ny
+        new_r_v[i] += [dx, dy]
+
+    return periodic_bc(new_r_v)
+
+#Movement is done by the velocity in the direction of the oscillator angle
 def move(r_v, th):
     new_r_v = np.copy(r_v)
     for i in range(len(r_v)):
@@ -20,7 +34,7 @@ def move(r_v, th):
 
     return periodic_bc(new_r_v)
 
-
+#Neighbors are two closest nodes
 def kuramoto_int(th, neigh):
     new_th = np.copy(th)
     for i in range(len(th)):
@@ -29,6 +43,7 @@ def kuramoto_int(th, neigh):
         new_th[i] += dt*K1/2*(np.sin(th_l-th[i]) + np.sin(th_r-th[i])) + K2/2 * (np.sin(2*th_r-th_l-th[i])+np.sin(2*th_l-th_r+th[i]))
     return new_th
 
+#N to N connections of oscillators
 def kuramoto_global(th):
     new_th = np.copy(th)
     for i in range(len(th)):
@@ -36,7 +51,7 @@ def kuramoto_global(th):
             new_th[i] += dt*K1/N*(np.sin(th[j]-th[i]))
     return new_th
 
-#Detects if particle| is at boundary and implements periodic boundary conditions
+#Detects if particle is at boundary and implements periodic boundary conditions
 def periodic_bc(r_new):
     #Detect boundary crossing and implement PBC
     for p in range(N):
@@ -52,7 +67,6 @@ def periodic_bc(r_new):
 def initialize_r0():
     r0_v = np.random.rand(N,2)*Lx #Create two random numbers between [0,1)
     th_0 = np.random.uniform(-np.pi, np.pi, N)
-
     return r0_v, th_0
 
 #Find two closest particles of each particle
@@ -72,14 +86,16 @@ def find_neighbors(r_v):
         neigh[i,1] = np.argmin(temp)
     return neigh
 
+#Produces animation only
 def animate():
     fig, ax = plt.subplots()
     ax.set_xlim(0, Lx)
     ax.set_ylim(0, Lx)
     ax.set_aspect('equal', adjustable='box')
-    sc = ax.scatter(r_0[:, 0], r_0[:, 1], c=np.sin(th_0), cmap="viridis", edgecolor="black")
+    normalize = colors.Normalize(vmin=-1, vmax=1)
+    sc = ax.scatter(r_0[:, 0], r_0[:, 1], c=np.sin(th_0), norm = normalize, cmap="viridis", edgecolor="black", s = 100)
     cb = plt.colorbar(sc)
-    # --- Animation update function ---
+
     def update(frame):
         global th_0, r_0, neigh
         th_0 = kuramoto_int(th_0,neigh)
@@ -92,45 +108,100 @@ def animate():
         sc.set_array(ang_c)
         return sc,
 
-    # --- Run animation ---
-    ani = animation.FuncAnimation(fig, update, frames=200, interval=100, blit=True)
+    ani = animation.FuncAnimation(fig, update, frames=200, interval=20, blit=True)
     plt.show()
 
-def animate_from_data():
+#For animating the produced act_mat
+def animate_from_data(filename = None):
     fig, ax = plt.subplots()
     ax.set_xlim(0, Lx)
     ax.set_ylim(0, Lx)
     ax.set_aspect('equal', adjustable='box')
 
-    sc = ax.scatter(pos_mat[:, 0, 0], pos_mat[:, 1, 0],
-                    c=np.sin(act_mat[:, 0]), cmap="viridis", edgecolor="black")
-    plt.colorbar(sc)
+    normalize = colors.Normalize(vmin=-1, vmax=1)
+    sc = ax.scatter(pos_mat[:, 0, 0], pos_mat[:, 1, 0], c=np.sin(act_mat[:, 0]), norm= normalize, cmap="viridis", edgecolor="black", s = 100)
+    cb = plt.colorbar(sc)
+    cb.set_label(r'sin($\theta$)')
 
     def update(frame):
-        #sc.set_offsets(pos_mat[:, :, frame])
+        sc.set_offsets(pos_mat[:, :, frame])
         sc.set_array(np.sin(act_mat[:, frame]))
         return sc,
 
-    ani = animation.FuncAnimation(fig, update, frames=act_mat.shape[1], interval=50, blit=True)
-    plt.show()
+    ani = animation.FuncAnimation(fig, update, frames=act_mat.shape[1], interval=20, blit=True)
 
-r_0, th_0 = initialize_r0()
-neigh = find_neighbors(r_0)
-act_mat = np.reshape(th_0, (N,1))
-pos_mat = np.zeros((N, 2, len(t)+1))
-pos_mat[:, :, 0] = r_0
+    if filename is not None:
+        ani.save(filename = "../"+filename + "anim.gif", writer = "pillow")
+    #plt.show()
 
-for i,_ in enumerate(t):
-    th_0 = kuramoto_global(th_0)#, neigh)
-    r_0 = move(r_0,th_0)
+def plot_from_data(filename = None):
+    fig, ax = plt.subplots(ncols=3, nrows=1, figsize=(15, 5),
+                         subplot_kw={
+                             "ylim": (0, Lx),
+                             "xlim": (0, Lx),
+                             "aspect": "equal",
+                             "adjustable": "box"})
+
+    times = [0, int(act_mat.shape[1]/2), int(act_mat.shape[1])-1]
+    normalize = colors.Normalize(vmin=-1, vmax=1)
+
+    for ax, time in zip(ax, times):
+        sc = ax.scatter(pos_mat[:, 0, time],
+                pos_mat[:, 1, time],
+                c=np.sin(act_mat[:, time]), cmap="viridis", norm = normalize, edgecolor="black", s = 100)
+        ax.set_title(f'Time = {time}')
+
+
+    fig.subplots_adjust(right=0.8)
+    cb_ax = fig.add_axes([0.85, 0.15, 0.02, 0.7])
+
+    cb = fig.colorbar(sc, cax=cb_ax)
+    cb.set_label(r'sin($\theta$)')
+
+    plt.suptitle(f"Time evol. for system with $K_1$= {K1} , $K_2$ = {K2} and $v_0$ = {v}" )
+
+    if filename is not None:
+        plt.savefig("../"+filename+"_seq.png")
+
+    #plt.show()
+
+
+N= 100#number of particles
+T = 150
+Lx = 10 #Box size
+K1_v = [1,0,1,3,1] #Pairwise interaction parameter
+K2_v = [0,1,1,1,3] #Trio-wise interaction parameter
+dt = 0.03 #Integration step
+v = 1 #Internal particle velocity
+gamma = 10
+t = np.linspace(0,T,int(T/dt))
+
+#kplt.animate_oscillators(act_mat)
+
+for j in range(len(K1_v)):
+    K1 = K1_v[j]
+    K2 = K2_v[j]
+
+    r_0, th_0 = initialize_r0()
     neigh = find_neighbors(r_0)
-    pos_mat[:, :, i+1] = r_0
-    act_mat = np.column_stack((act_mat,th_0))
+    act_mat = np.reshape(th_0, (N,1))
+    pos_mat = np.zeros((N, 2, len(t)+1))
+    pos_mat[:, :, 0] = r_0
 
-kplt.plot_phase_coherence(act_mat)
-kplt.animate_oscillators(act_mat)
+    for i,_ in enumerate(t):
+        th_0 = kuramoto_int(th_0, neigh)
+        r_0 = move_noise(r_0,th_0)
+        neigh = find_neighbors(r_0)
+        pos_mat[:, :, i+1] = r_0
+        act_mat = np.column_stack((act_mat,th_0))
 
-animate_from_data()
+    filename = "K1_"+str(K1)+"_K2_"+str(K2)+"_v_"+str(v)
+    kplt.plot_phase_coherence(act_mat,filename)
+    animate_from_data(filename)
+    plot_from_data(filename)
 
+#kplt.plot_phase_coherence(act_mat)
+#animate_from_data()
+#plot_from_data()
 
 #animate()
